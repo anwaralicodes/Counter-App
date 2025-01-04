7f2efff9523c27ee3efec0cdbd20c242d43bd378
anwaralicodes <anwaralicodes@gmail.com>
2025-01-04 22:13:06 +0000

    counter app finished

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 const incrementBtn = document.getElementById("increment");
 const decrementBtn = document.getElementById("decrement");
 const resetBtn = document.getElementById("reset");
+const countLabel = document.getElementById("countLabel");
+let count = 0;
+
+incrementBtn.onclick = function () {
+  count++;
+  countLabel.textContent = count;
+};
+
+decrementBtn.onclick = function () {
+  if (count > 0) {
+    count--;
+    countLabel.textContent = count;
+  }
+};
+
+resetBtn.onclick = function () {
+  count = 0;
+  countLabel.textContent = count;
+};
